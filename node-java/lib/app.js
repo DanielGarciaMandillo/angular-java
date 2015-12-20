92539e9155f8fbe7583672310c165f26d502c5f7
Daniel Garcia <daniel.garciamandillo@gmail.com>
2015-12-20 21:14:39 +0100

    Added java maven project

--- node-java/lib/app.js
+++ node-java/lib/app.js
@@ -1,12 +1,12 @@
 var java = require("java");
 
-java.classpath.push("jar/test.jar");
+java.classpath.push("bin/electron-node-java-0.0.1.jar");
 
 //Llamada a instancia util random
 var math = java.newInstanceSync("java.util.Random");
-var result1 = (math.nextDoubleSync()*20).toFixed(0);
+var result1 = (math.nextDoubleSync()*10).toFixed(0);
 document.getElementById("result1").textContent = result1;
 
 //Llamada a getRandomInteger
-var result2 = java.callStaticMethodSync("test.Main", "getRandomInteger");
+var result2 = java.callStaticMethodSync("com.random.Main", "getRandomInteger");
 document.getElementById("result2").textContent = result2;
---
+++ src/com/random/Main.java
@@ -0,0 +1,9 @@
+package com.random;
+
+public class Main {
+
+	public static int getRandomInteger() {
+		return (int) (Math.random() * 10);
+	}
+
+}
