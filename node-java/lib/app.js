885c8e4dc384d1a546758726f97dfaf37659b2e5
Daniel Garcia <daniel.garciamandillo@gmail.com>
2015-12-22 09:49:23 +0100

    Added node-java-maven dependencies

--- node-java/lib/app.js
+++ node-java/lib/app.js
@@ -1,4 +1,16 @@
-var java = require("java");
+var java = require('java');
+var mvn = require('node-java-maven');
+
+mvn(function (err, mvnResults) {
+  if (err) {
+    return console.error('could not resolve maven dependencies', err);
+  }
+  mvnResults.classpath.forEach(function (c) {
+    console.log('adding ' + c + ' to classpath');
+    java.classpath.push(c);
+  });
+
+});
 
 java.classpath.push("bin/electron-node-java-0.0.1.jar");
 
--- node-java/main.js
+++ node-java/main.js
@@ -1,37 +1,21 @@
-var app = require('app');  // Módulo para controlar el ciclo de vida de la aplicación.
-var BrowserWindow = require('browser-window');  // Módulo para crear uan ventana de navegador.
-
-// Mantener una referencia global al objeto window, si no lo haces, esta ventana
-// se cerrará automáticamente cuando el objeto JavaScript sea recolectado (garbage collected):
+var app = require('app');
+var BrowserWindow = require('browser-window');
 var mainWindow = null;
 
-// Salir de todas las ventanas cuando se cierren.
 app.on('window-all-closed', function() {
-  // En OS X es común que las aplicaciones y su barra de menú
-  // se mantengan activas hasta que el usuario cierre la aplicación
-  // explícitamente utilizando Cmd + Q
   if (process.platform != 'darwin') {
     app.quit();
   }
 });
 
-// Este método será llamado cuando Electron haya finalizado la inicialización
-// y esté listo para crear ventanas de navegador.
 app.on('ready', function() {
-  // Crear la ventana.
   mainWindow = new BrowserWindow({width: 1280, height: 1024});
 
-  // cargar el index.html de nuestra aplicación.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Desplegar devtools.
   mainWindow.openDevTools();
 
-  // Evento emitido cuando se cierra la ventana.
   mainWindow.on('closed', function() {
-    // Eliminar la referencia del objeto window.
-    // En el caso de soportar multiples ventanas, es usual almacenar
-    // los objetos window en un array, este es el momento en el que debes eliminar el elemento correspondiente.
     mainWindow = null;
   });
 });
