0c1332f34ea0561a87c4b693216f0feca28e544f
Daniel Garcia <daniel.garciamandillo@gmail.com>
2015-12-23 16:04:51 +0100

    Modified package name

--- node-java/lib/app.js
+++ node-java/lib/app.js
@@ -1,7 +1,7 @@
 var java = require('java');
 java.classpath.push("bin/electron-node-java-0.0.1-jar-with-dependencies.jar");
 
-var repository = java.newInstanceSync('main.todo.Repository');
+var repository = java.newInstanceSync('com.todo.Repository');
 
 initApp();
 
@@ -15,7 +15,7 @@ function addTask() {
   var name = document.getElementById('task').value;
   if (name) {
     document.getElementById('task').value = '';
-    var task = java.newInstanceSync('main.todo.Item', name);
+    var task = java.newInstanceSync('com.todo.Item', name);
     repository.insertItemSync(task);
     getAllTasks();
   }
