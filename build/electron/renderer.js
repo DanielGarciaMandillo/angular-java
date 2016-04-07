a3b093aa339efddc1b98b34c25f765e7dc795e6b
Daniel Garcia <daniel.garciamandillo@gmail.com>
2016-04-07 13:54:41 +0200

    deleted build folder

--- build/electron/renderer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-//public angular electron API for renderer (UI/browser)
-__export(require('angular2/core'));
-__export(require('./platform/electron_renderer'));
