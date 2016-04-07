15d0df506426ad5200c76d6e607a8df893db48b8
Daniel Garcia <daniel.garciamandillo@gmail.com>
2016-04-07 13:27:38 +0200

    java project added
    Former-commit-id: e953e953945cfc8fa8f5408ba572fada78340795

---
+++ src/main/java/com/todo/Item.java
@@ -0,0 +1,18 @@
+package com.todo;
+
+public class Item {
+	private String name;
+
+	public Item(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
