dd2c517433cc0572a2f549c49f6f1ff359d91921
Daniel Garcia <daniel.garciamandillo@gmail.com>
2016-04-07 13:54:41 +0200

    deleted build folder
    Former-commit-id: e2af84b9ad91b227beba1539aec7b54f49ed2068

--- build/app_ui.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-require('reflect-metadata');
-var renderer_1 = require('./electron/renderer');
-renderer_1.bootstrapElectronRenderer();
--- build/electron/platform/electron.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-var electron_app_1 = require('./electron_app');
-exports.bootstrap = electron_app_1.bootstrap;
